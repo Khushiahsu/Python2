b9995f013fe2c5b9779e1a6e0bb3bf5d9a98e174
Khushiahsu <113340388+Khushiahsu@users.noreply.github.com>
2024-10-31 16:08:49 +0000

    Add files via upload

---
+++ Activity4.py
@@ -0,0 +1,59 @@
+num=50
+print(type(num))
+
+x=69.32
+z=str(x)
+print(type(x))
+
+at = 'one'
+print(type(at))
+
+x = False
+print(type(x))
+
+
+
+name = 'Aarav'
+age = 10
+temperature = 23.54
+#typecasting is converting one data type into another
+print(" Integer Value in ",type(age)) #int
+age=str(age)
+print("String value in ", type(age)) #string
+print(type(temperature))# float
+
+temperature =  int(temperature)
+print(type(temperature))#integer
+
+
+
+name_input=input("Enter your name here : ")
+print(name_input)
+hobby_input=input('What is your favorite hobby?')
+print(hobby_input)
+ 
+
+
+ #<-----COMMONLY USED OPERATION---->
+
+ #lower():-converts into lowercase
+
+ #upper(),split(),join(),find(),replace()
+
+ #  a a r a v
+ #  0 1 2 3 4 
+
+ # slicing is obtaining substring from the string 
+
+ # (startindex, end index+1)
+
+
+str1="codingal"
+print(str1[:3])
+print(str1[3:])
+print(str1[::-1])
+
+coun = 'Russia'
+print(coun[::-1])
+
+
